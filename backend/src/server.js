9dbc4c62686e714b2f5ab9fb2163b543eef7aeab
Tiago D. Teixeira <tiagoadmstz@yahoo.com.br>
2019-08-06 13:13:19 -0300

    teste

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-    return res.send(`Hello ${req.query.name}`);
+    return res.json({message: `Hello ${req.query.name}`});
 });
 
 server.listen(3333);
