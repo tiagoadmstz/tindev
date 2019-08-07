c7be728996d7b7c56c0fe31422b8f080a8ed6e55
Tiago D. Teixeira <tiagoadmstz@yahoo.com.br>
2019-08-07 14:13:54 -0300

    add mongodb connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 const server = express();
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-jqf8u.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
 
 server.use(express.json());
 server.use(routes);
